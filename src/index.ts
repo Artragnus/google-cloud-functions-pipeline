a77d59557bf0a74dddb5c4897c89386f936ed4d5
João Paulo <joaopptt@hotmail.com>
2024-08-13 13:59:44 -0300

    refactor: add vulnerabilities to code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { HttpFunction } from "@google-cloud/functions-framework";
 
 export const main: HttpFunction = async (req, res) => {
+  console.log(process.env.TOKEN);
   return res.send("Hello, World!");
 };
