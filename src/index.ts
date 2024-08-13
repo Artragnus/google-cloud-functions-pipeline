fa974b1e7fc648223fe2298853a5ff898eb5092d
João Paulo <joaopptt@hotmail.com>
2024-08-13 14:04:24 -0300

    fix: token hardcoded

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { HttpFunction } from "@google-cloud/functions-framework";
 
 export const main: HttpFunction = async (req, res) => {
-  console.log(process.env.TOKEN);
   return res.send("Hello, World!");
 };
